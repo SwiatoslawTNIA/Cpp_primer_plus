e8e415b348f227c80499ea38d7bfbfee6dfe5930
Swiatoslaw <swiatoslaw.p@gmail.com>
2024-06-16 22:22:26 +0200

    Worked on some class implementations

--- sources/c++.cc
+++ sources/c++.cc
@@ -60,12 +60,12 @@ cout << "Time spent: " << (clock() - start_time) / CLOCKS_PER_SEC;
 void callme1(StringBad & rsb)
 {
   using std::cout;
-cout << "String passed by reference:\n";
-cout << "\"" << rsb << "\"\n";
+  cout << "String passed by reference:\n";
+  cout << "\"" << rsb << "\"\n";
 }
 void callme2(StringBad sb)
 {
   using std::cout;
-cout << "String passed by value:\n";
-cout << "\"" << sb << "\"\n";
+  cout << "String passed by value:\n";
+  cout << "\"" << sb << "\"\n";
 }
